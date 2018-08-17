579befe378dd7a942dda1db49bf3defe26abbec8
James Hammond <james@jamesjhammond.com>
2018-08-17 13:45:04 -0600

    Nearly complete react/redux app tutorial

---
+++ Psadmin/src/routes.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var React = require("react");
+var Router = require("react-router");
+var DefaultRoute = Router.DefaultRoute;
+var Route = Router.Route;
+var NotFoundRoute = Router.NotFoundRoute;
+
+var routes = (
+    <Route name="app" path="/" handler={require("./components/app")}>
+        <DefaultRoute handler={require("./components/homePage")} />
+        <NotFoundRoute handler={require("./components/notFoundPage")} />
+        <Route name="authorPage" handler={require("./components/authors/authorPage")} />
+        <Route name="about" handler={require("./components/about/aboutPage")} />
+    </Route>
+);
+
+module.exports = routes;
