ac19e0ea719a25a7386cf3093410f76aceef85ed
James Hammond <james@jamesjhammond.com>
2018-08-28 14:57:32 -0600

    Wiring up the manage courses page

---
+++ pluralsight-redux-starter-master/src/components/course/ManageCoursePage.js
@@ -0,0 +1,49 @@
+import React, {PropTypes} from "react";
+import {connect} from "react-redux";
+import {bindActionCreators} from "redux";
+import * as courseActions from "../../actions/courseActions";
+import CourseForm from "./CourseForm";
+
+class ManageCoursePage extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+          course: Object.assign({}, this.props.course),
+          errors: {}
+        };
+    }
+  
+    render() {
+        return (
+            <div>
+              <CourseForm
+                //onChange={}
+                //onSave={}
+                allAuthors={[]}
+                course={this.state.course}
+                errors={this.state.errors}/>
+            </div>
+        );
+    }
+}
+
+ManageCoursePage.propTypes = {
+    //myProp: PropTypes.string.isRequired
+  course: PropTypes.object.isRequired
+};
+
+function mapStateToProps(state, ownProps) {
+  let course = {id: "", watchHref: "", authorId: "", length: "", category: ""};
+  return {
+        course: course
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators(courseActions, dispatch)
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage);
